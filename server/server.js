a4a4c64e8d35844bef5ec9160764da0a39378b44
Han Nguyen <hanvnguyen94@gmail.com>
2021-07-25 14:46:45 -0400

    Initial commit;

--- server/server.js
+++ server/server.js
@@ -1,12 +1,57 @@
 const express = require('express')
-const spotifyWebApi = require('spotify-web-api-node')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const SpotifyWebApi = require('spotify-web-api-node')
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/refresh', (req, res) => {
+	const refreshToken = req.body.refreshToken
+	const spotifyApi = new SpotifyWebApi({
+		redirectUri: 'http://localhost:3000/callback/',
+		clientId: '12d199776a464ddf81299432a03ae8de',
+		clientSecret: 'c14f320c2c454e61a1a25775737223b4',
+		refreshToken,
+	})
+
+	spotifyApi
+		.refreshAccessToken()
+		.then((data) => {
+			console.log(data.body)
+
+			// Save the access token so that it's used in future calls
+			// spotifyApi.setAccessToken(data.body['access_token'])
+		})
+		.catch(() => {
+			res.sendStatus(400)
+		})
+})
 
 app.post('/login', (req, res) => {
+	const code = req.body.code
+
 	const spotifyApi = new SpotifyWebApi({
-		redirectUri: 'http://localhost:3000',
-		clientId: '5d69b4331ad54c85a9e6a91e4a89dff6',
-		clientSecret: '66a387a0c2e94f75ac9eae4d9bf69e05',
+		redirectUri: 'http://localhost:3000/callback/',
+		clientId: '12d199776a464ddf81299432a03ae8de',
+		clientSecret: 'c14f320c2c454e61a1a25775737223b4',
 	})
+
+	spotifyApi
+		.authorizationCodeGrant(code)
+		.then((data) => {
+			console.log(data.body)
+			// res.json({
+			// 	accessToken: data.body.access_token,
+			// 	refreshToken: data.body.refresh_token,
+			// 	expiresIn: data.body.expires_in,
+			// })
+		})
+		.catch((err) => {
+			console.log(err)
+			res.sendStatus(400)
+		})
 })
+
+app.listen(3001)
